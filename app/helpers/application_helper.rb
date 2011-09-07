d9030235c486c292658a739e7ae9ed60d89254b5
Garrison Prinslow <gprinslow@gmail.com>
2011-09-06 20:59:44 -0500

    Improve logo handling w/app helper; change h1 of signup page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 module ApplicationHelper
   
-  # Return a title on a per-page basis.
+	def logo
+		logo = image_tag("logo.png",  :alt => "Youpon", :class => "round")
+		if @logo.nil?
+			logo
+		else
+			"Youpon"
+		end
+	end
+  
+	# Return a title on a per-page basis.
   def title
     base_title = "Youpon"
     if @title.nil?
