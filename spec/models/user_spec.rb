4b60e3155a34817affc9df728d2a6c86b3f3b696
Garrison Prinslow <gprinslow@gmail.com>
2011-09-10 13:17:14 -0500

    Add tests for encryped password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -120,4 +120,15 @@ describe User do
     end
   end
 
+	describe "password encryption" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+  end
+
 end
