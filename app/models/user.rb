2b4783bd9e413953378f0abba04c56c2e5251760
Garrison Prinslow <gprinslow@gmail.com>
2011-09-10 11:47:57 -0500

    Some changes to user model and spec in preparation of password

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,16 +18,12 @@
 
 class User < ActiveRecord::Base
   has_many :redemptions
-	attr_accessible :username, :email, :password, :nameFirst, :nameMiddle, :nameLast, :birthday, :gender, :zipCode
+	attr_accessor :password
+	attr_accessible :username, :email, :password, :password_confirmation, :nameFirst, :nameMiddle, :nameLast, :birthday, :gender, :zipCode
 	
 	email_regex =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
-	validates :username, :presence => true, 
-												:length => { :maximum => 50 },
-												:uniqueness => { :case_sensitive => false }
-	validates :email, :presence => true,
-												:format => { :with => email_regex },
-												:uniqueness => { :case_sensitive => false }
-	validates :password, :presence => true
-	
+	validates :username, :presence => true, :length => { :maximum => 50 }, :uniqueness => { :case_sensitive => false }
+	validates :email, :presence => true, :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false }
+	validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }
 end
