7735da16aa1b7278f79b408d5ffcdc1021350087
Garrison Prinslow <gprinslow@gmail.com>
2011-09-10 13:20:14 -0500

    Add encryped_password to users, reannotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,18 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  username   :string(255)
-#  email      :string(255)
-#  nameFirst  :string(255)
-#  nameMiddle :string(255)
-#  nameLast   :string(255)
-#  birthday   :date
-#  gender     :string(255)
-#  zipCode    :integer
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  username           :string(255)
+#  email              :string(255)
+#  nameFirst          :string(255)
+#  nameMiddle         :string(255)
+#  nameLast           :string(255)
+#  birthday           :date
+#  gender             :string(255)
+#  zipCode            :integer
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110910165002) do
+ActiveRecord::Schema.define(:version => 20110910181804) do
 
   create_table "customers", :force => true do |t|
     t.integer  "user_id"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(:version => 20110910165002) do
     t.integer  "zipCode"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
