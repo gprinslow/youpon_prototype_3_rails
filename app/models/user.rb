6c93ef248fc6fa97deda8af0289f468a461e519c
Garrison Prinslow <gprinslow@gmail.com>
2011-09-10 14:09:22 -0500

    Encrypt password with salt

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@
 #  encrypted_password :string(255)
 #
 
+require 'digest'
 class User < ActiveRecord::Base
   has_many :redemptions
 	attr_accessor :password
@@ -39,11 +40,20 @@ class User < ActiveRecord::Base
 	private
 		
 		def encrypt_password
-			self.encrypted_password = encrypt(self.password)
+			self.salt = make_salt if new_record?
+			self.encrypted_password = encrypt(password)
 		end
 		
 		def encrypt(string)
-			string #Only temporary
+			secure_hash("#{salt}--#{string}")
+		end
+		
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+		
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
 		end
 		
 end
