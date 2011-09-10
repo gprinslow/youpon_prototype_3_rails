786d83e8d519cea62595ea2db8cb57a61ab5e3a2
Garrison Prinslow <gprinslow@gmail.com>
2011-09-10 12:22:12 -0500

    Annotations changed for remove password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@
 #  id         :integer         not null, primary key
 #  username   :string(255)
 #  email      :string(255)
-#  password   :string(255)
 #  nameFirst  :string(255)
 #  nameMiddle :string(255)
 #  nameLast   :string(255)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  username   :string(255)
+#  email      :string(255)
+#  nameFirst  :string(255)
+#  nameMiddle :string(255)
+#  nameLast   :string(255)
+#  birthday   :date
+#  gender     :string(255)
+#  zipCode    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe User do
