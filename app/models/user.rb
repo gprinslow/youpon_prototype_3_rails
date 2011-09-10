49324503edd4c69f822f15e1a599ea3d81383da9
Garrison Prinslow <gprinslow@gmail.com>
2011-09-10 13:59:41 -0500

    Thru 7.2.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,13 @@ class User < ActiveRecord::Base
 	
 	before_save :encrypt_password
 	
+	#Return true if the user's password matches the submitted password
+	def has_password?(submitted_password)
+		#Compare encrypted_password with encrypt(submitted_password)
+		encrypted_password == encrypt(submitted_password)
+	end
+	
+	
 	private
 		
 		def encrypt_password
