91e2388cf67e532752d81c891122ab017cb8123e
Garrison Prinslow <gprinslow@gmail.com>
2011-09-11 22:46:51 -0500

    User Sign up finished, links make sense, new flash notification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
+        format.html { flash[:success] = "Welcome to Youpon!"; redirect_to(@user) }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         @title = "Sign up"
