525fe7efca61283da20e3bb26cda44af5180a4d6
Garrison Prinslow <gprinslow@gmail.com>
2011-09-16 15:03:51 -0500

    Improve reports; filters out non-managers properly

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,22 +1,28 @@
 class ReportsController < ApplicationController
   def index
-
-	    @title = "Reports"
-
+		@title = "Reports"
+		
+		if current_user
 			@employee = Employee.find_by_user_id(current_user.id)
-			
-		#	if @employee != nil
-				#@manager = Manager.find_by_employee_id(@employee.id)
-				#if @manager != nil
-					@merchant = Merchant.find_by_id(1) # TODO: FIX THIS RELATIONSHIP (thru Roster?)
-					@offers = @merchant.offers
-				#end
-			#end
 
-	    respond_to do |format|
-	      format.html # index.html.erb
-	    end
-
+			if @employee
+				@roster = Roster.find_by_employee_id(@employee.id)
+				@merchant = Merchant.find_by_id(@roster.merchant_id)
+				@offers = @merchant.offers
+				
+				@manager = Manager.find_by_employee_id(@employee.id)
+				if @manager
+					respond_to do |format|
+						format.html
+					end
+				else
+					redirect_to root_path
+				end
+			else
+				redirect_to root_path
+			end
+		else
+			redirect_to root_path
+		end
   end
-
 end
