84f12f0ffa972dc1478cc5142c368c3712842385
Garrison Prinslow <gprinslow@gmail.com>
2011-10-09 18:35:22 -0500

    Add json support to sessions for iPhone login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,36 @@
 class SessionsController < ApplicationController
   def new
-		@title = "Sign in"
+		respond_to do |format|
+			format.html { @title = "Sign in" }
+			format.json { head :ok }
+		end
   end
 
 	def create
     user = User.authenticate(params[:session][:username],
                              params[:session][:password])
     if user.nil?
-      flash.now[:error] = "Invalid username/password combination."
-      @title = "Sign in"
-      render 'new'
-    else
+      respond_to do |format|
+				format.html {
+					flash.now[:error] = "Invalid username/password combination."
+		      @title = "Sign in"
+		      render 'new'
+				}
+				format.json {
+					render :json => { :action => 'login', :error => 'invalid username/password' }
+				}
+    	end
+		else
 			sign_in user
-			flash[:success] = "Welcome back to Youpon!"
-			redirect_to user
+			respond_to do |format|
+				format.html {
+					flash[:success] = "Welcome back to Youpon!"
+					redirect_to user
+				}
+				format.json {
+					render :json => { :action => 'login', :owner => current_user }
+				}
+			end
     end
 
 	end
